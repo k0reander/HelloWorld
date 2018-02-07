cd67bee908fa9c959794fbc64ae5826968d874a1
Frank Dupont <k0reander@yahoo.com>
2018-02-07 11:44:34 +0100

    fixed unchecked warning by fixing raw Pair instantiation

--- src/main/java/generics/wildcards/Methods.java
+++ src/main/java/generics/wildcards/Methods.java
@@ -12,7 +12,7 @@ public class Methods {
 
     public static void main(String[] args) {
         Pair<?,?> pair1 = new Pair(new Dog(), new Cat());
-        Pair<Dog, Dog> pair2 = new Pair(new Dog(), new Dog());
+        Pair<Dog, Dog> pair2 = new Pair<>(new Dog(), new Dog());
         Methods.printPair( pair1 );
         Methods.printPair( pair2);
     }
