c8a322ded105135b9320ed462000f5cd97e3618c
Frank Dupont <k0reander@yahoo.com>
2018-02-07 13:46:46 +0100

    added raw types to Fields

--- src/main/java/generics/fields/Fields.java
+++ src/main/java/generics/fields/Fields.java
@@ -2,6 +2,7 @@ package generics.fields;
 
 import generics.model.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Fields {
@@ -19,5 +20,27 @@ public class Fields {
         Pair<Human, Human>[] couples = new Marriage[2];
         //Allawys do
         Marriage[] marriages = new Marriage[2];
+
+        List<String> words = new ArrayList<>();
+
+        acceptList(words);
+        acceptWildcardList(words);
+
+        List raw = new ArrayList();
+        acceptStringList(raw);
+
+        List<?> unbounded = null;
+        //acceptStringList(unbounded);
     }
+
+    static void acceptWildcardList(List<?> unbounded){
+        if(unbounded instanceof List<?>)
+            System.out.println();
+        //unbounded.add(new Object());
+    }
+    static void acceptList(List raw){
+        raw.add(new Object());
+    }
+
+    static void acceptStringList(List<String> l){}
 }
