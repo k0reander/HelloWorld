e09e1cea6457226e1395b5b725bd5b20614c12ad
Frank Dupont <k0reander@yahoo.com>
2018-02-08 11:06:29 +0100

    added ? super example to TreeMap

--- src/main/java/generics/wildcards/MyTreeMap.java
+++ src/main/java/generics/wildcards/MyTreeMap.java
@@ -1,5 +1,6 @@
 package generics.wildcards;
 
+import java.util.Collection;
 import java.util.TreeMap;
 
 public class MyTreeMap<K extends Comparable<K>, D> {
@@ -7,6 +8,10 @@ public class MyTreeMap<K extends Comparable<K>, D> {
 
     public void add(K key, D data){
         tree.put(key, data);
+    }
+
+    public void fill(Collection<? super D> collection){
+        collection.addAll(tree.values());
     }
 
     public static void main(String[] args) {
